ba52389c67b5aac719782e4a09af40426500df15
isan-tim <isantim7@gmail.com>
2021-03-17 08:29:47 +0300

    Выполнено задание 1

---
+++ src/main/java/ru/geekbrains/Homework1.java
@@ -0,0 +1,10 @@
+package ru.geekbrains;
+///*
+// * Задание №1
+// * Создать пустой проект в IntelliJ IDEA и прописать метод main().
+// */
+public class Homework1 {
+    public static void main (String [] args)
+    {
+    }
+}
