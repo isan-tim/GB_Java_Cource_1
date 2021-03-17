843fbdcebed14d54359ba5526893b465e7134b12
isan-tim <isantim7@gmail.com>
2021-03-17 08:34:23 +0300

    Выполнено задание 2

--- src/main/java/ru/geekbrains/Homework1.java
+++ src/main/java/ru/geekbrains/Homework1.java
@@ -1,10 +1,33 @@
 package ru.geekbrains;
+
 ///*
 // * Задание №1
 // * Создать пустой проект в IntelliJ IDEA и прописать метод main().
 // */
+//public class Homework1 {
+//    public static void main (String [] args)
+//    {
+//    }
+//}
+
+// * Задание №2
+// * Создать переменные всех пройденных типов данных и инициализировать их значения.
+// */
 public class Homework1 {
     public static void main (String [] args)
     {
+//        Целочисленные значения
+        int a = 1;
+        byte b = 2;
+        short c = 3;
+        long d = 4;
+//        С плавающей точкой
+        float e = 2.8f;
+        double f = 2.8;
+//        Символьные
+        char g = 'a';
+//        Логический тип
+        boolean isTrue = true;
+        boolean isFalse = false;
     }
 }
